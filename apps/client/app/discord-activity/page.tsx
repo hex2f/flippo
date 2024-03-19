f81cda772e194f678c3d0480857cff1ac842f3db
Leah Lundqvist <leah@pigeon.sh>
2024-03-19 18:11:52 +0100

    use dynamic import for discord sdk

--- apps/client/app/discord-activity/page.tsx
+++ apps/client/app/discord-activity/page.tsx
@@ -1,7 +1,14 @@
-import DiscordActivitySDK from "./_discordsdk";
+import dynamic from 'next/dynamic';
+
+const DiscordActivitySDK = dynamic(
+	() => import('@/components/_discordsdk'),
+	{ ssr: false }
+);
 
 export default async function DiscordActivityPage() {
 	return (
-		<DiscordActivitySDK />
+    <main className="flex flex-col items-center justify-center flex-1">
+			<DiscordActivitySDK />
+		</main>
 	)
 }
